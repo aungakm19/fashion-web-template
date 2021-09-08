5a49b00954c619f8bf02e68231f1e29acd336ced
aungakm19 <kmyat1521@gmail.com>
2021-09-08 22:06:36 +0630

    first commit

---
+++ main.js
@@ -0,0 +1,344 @@
+let a = document.querySelector("#btn")
+	a.onclick = function () {
+	let body = document.querySelector("body")
+	body.classList.toggle("dark-mode")
+	let nav = document.querySelector("nav")
+	nav.classList.toggle("navbgdark")
+	let bgcolor = document.querySelector("#item")
+	bgcolor.classList.toggle("dark-mode")
+
+	let table1 = document.querySelector("#table1")
+	table1.classList.toggle("dark-mode1")
+	let table2 = document.querySelector("#table2")
+	table2.classList.toggle("dark-mode1")
+
+
+	let contact1 = document.querySelector("#contact1")
+	contact1.classList.toggle("dark-bg")
+	contact1.classList.toggle("block")
+	let contact2 = document.querySelector("#contact2")
+	contact2.classList.toggle("dark-bg")
+	contact2.classList.toggle("block")
+	let contact3 = document.querySelector("#contact3")
+	contact3.classList.toggle("dark-bg")
+	contact3.classList.toggle("block")
+	let contact4 = document.querySelector("#contact4")
+	contact4.classList.toggle("dark-bg")
+	contact4.classList.toggle("block")
+	
+	let copyright = document.querySelector("#copyright")
+	copyright.classList.toggle("copyright-dark")
+	copyright.classList.toggle("bg")
+}
+
+
+
+let allbtn = document.querySelector("#btn1")
+allbtn.onclick = function () {
+	// let item1 = document.querySelector("#item1")
+	// item1.style.display = "inline"
+	// let item2 = document.querySelector("#item2")
+	// item2.style.display = "inline"
+	// let item3 = document.querySelector("#item3")
+	// item3.style.display = "inline"
+	// let item4 = document.querySelector("#item4")
+	// item4.style.display = "inline"
+	// let item5 = document.querySelector("#item5")
+	// item5.style.display = "inline"
+	// let item6 = document.querySelector("#item6")
+	// item6.style.display = "inline"
+	// let item7 = document.querySelector("#item7")
+	// item7.style.display = "inline"
+	// let item8 = document.querySelector("#item8")
+	// item8.style.display = "inline"
+	// let item9 = document.querySelector("#item9")
+	// item9.style.display = "inline"
+	// let item10 = document.querySelector("#item10")
+	// item10.style.display = "inline"
+	// let item11 = document.querySelector("#item11")
+	// item11.style.display = "inline"
+	// let item12 = document.querySelector("#item12")
+	// item12.style.display = "inline"
+	// let item13 = document.querySelector("#item13")
+	// item13.style.display = "inline"
+	// let item14 = document.querySelector("#item14")
+	// item14.style.display = "inline"
+	// let item15 = document.querySelector("#item15")
+	// item15.style.display = "inline"
+	// let item16 = document.querySelector("#item16")
+	// item16.style.display = "inline"
+	// let item17 = document.querySelector("#item17")
+	// item17.style.display = "inline"
+	// let item18 = document.querySelector("#item18")
+	// item18.style.display = "inline"
+	// let item19 = document.querySelector("#item19")
+	// item19.style.display = "inline"
+	// let item20 = document.querySelector("#item20")
+	// item20.style.display = "inline"
+
+	allbtn.style.background = "white"
+	womanbtn.style.background = "none"
+	popularbtn.style.background = "none"
+	latestbtn.style.background = "none"
+	discountbtn.style.background = "none"
+	manbtn.style.background = "none"
+}
+
+let popularbtn = document.querySelector("#btn2")
+popularbtn.onclick = function () {
+// 	// document.querySelector(".items").style.transition = "all 2s";
+// 	let item1 = document.querySelector("#item1")
+// 	item1.style.display = "none"
+// 	let item2 = document.querySelector("#item2")
+// 	item2.style.display = "none"
+// 	let item3 = document.querySelector("#item3")
+// 	item3.style.display = "none"
+// 	let item4 = document.querySelector("#item4")
+// 	item4.style.display = "none"
+// 	let item5 = document.querySelector("#item5")
+// 	item5.style.display = "none"
+// 	let item6 = document.querySelector("#item6")
+// 	item6.style.display = "none"
+// 	let item7 = document.querySelector("#item7")
+// 	item7.style.display = "none"
+// 	let item8 = document.querySelector("#item8")
+// 	item8.style.display = "inline"
+// 	let item9 = document.querySelector("#item9")
+// 	item9.style.display = "inline"
+// 	let item10 = document.querySelector("#item10")
+// 	item10.style.display = "inline"
+// 	let item11 = document.querySelector("#item11")
+// 	item11.style.display = "none"
+// 	let item12 = document.querySelector("#item12")
+// 	item12.style.display = "none"
+// 	let item13 = document.querySelector("#item13")
+// 	item13.style.display = "none"
+// 	let item14 = document.querySelector("#item14")
+// 	item14.style.display = "none"
+// 	let item15 = document.querySelector("#item15")
+// 	item15.style.display = "none"
+// 	let item16 = document.querySelector("#item16")
+// 	item16.style.display = "inline"
+// 	let item17 = document.querySelector("#item17")
+// 	item17.style.display = "inline"
+// 	let item18 = document.querySelector("#item18")
+// 	item18.style.display = "inline"
+// 	let item19 = document.querySelector("#item19")
+// 	item19.style.display = "none"
+// 	let item20 = document.querySelector("#item20")
+// 	item20.style.display = "none"
+
+	popularbtn.style.background = "white"
+	womanbtn.style.background = "none"
+	allbtn.style.background = "none"
+	latestbtn.style.background = "none"
+	discountbtn.style.background = "none"
+	manbtn.style.background = "none"
+}
+
+let latestbtn = document.querySelector("#btn3")
+latestbtn.onclick = function () {
+// 	let item1 = document.querySelector("#item1")
+// 	item1.style.display = "block"
+// 	let item2 = document.querySelector("#item2")
+// 	item2.style.display = "block"
+// 	let item3 = document.querySelector("#item3")
+// 	item3.style.display = "block"
+// 	let item4 = document.querySelector("#item4")
+// 	item4.style.display = "block"
+// 	let item5 = document.querySelector("#item5")
+// 	item5.style.display = "none"
+// 	let item6 = document.querySelector("#item6")
+// 	item6.style.display = "none"
+// 	let item7 = document.querySelector("#item7")
+// 	item7.style.display = "none"
+// 	let item8 = document.querySelector("#item8")
+// 	item8.style.display = "none"
+// 	let item9 = document.querySelector("#item9")
+// 	item9.style.display = "none"
+// 	let item10 = document.querySelector("#item10")
+// 	item10.style.display = "none"
+// 	let item11 = document.querySelector("#item11")
+// 	item11.style.display = "none"
+// 	let item12 = document.querySelector("#item12")
+// 	item12.style.display = "none"
+// 	let item13 = document.querySelector("#item13")
+// 	item13.style.display = "none"
+// 	let item14 = document.querySelector("#item14")
+// 	item14.style.display = "block"
+// 	let item15 = document.querySelector("#item15")
+// 	item15.style.display = "none"
+// 	let item16 = document.querySelector("#item16")
+// 	item16.style.display = "none"
+// 	let item17 = document.querySelector("#item17")
+// 	item17.style.display = "none"
+// 	let item18 = document.querySelector("#item18")
+// 	item18.style.display = "none"
+// 	let item19 = document.querySelector("#item19")
+// 	item19.style.display = "block"
+// 	let item20 = document.querySelector("#item20")
+// 	item20.style.display = "block"
+
+
+	latestbtn.style.background = "white"
+	womanbtn.style.background = "none"
+	popularbtn.style.background = "none"
+	allbtn.style.background = "none"
+	discountbtn.style.background = "none"
+	manbtn.style.background = "none"
+}
+
+let discountbtn = document.querySelector("#btn4")
+discountbtn.onclick = function () {
+// 	let item1 = document.querySelector("#item1")
+// 	item1.style.display = "none"
+// 	let item2 = document.querySelector("#item2")
+// 	item2.style.display = "none"
+// 	let item3 = document.querySelector("#item3")
+// 	item3.style.display = "none"
+// 	let item4 = document.querySelector("#item4")
+// 	item4.style.display = "none"
+// 	let item5 = document.querySelector("#item5")
+// 	item5.style.display = "none"
+// 	let item6 = document.querySelector("#item6")
+// 	item6.style.display = "block"
+// 	let item7 = document.querySelector("#item7")
+// 	item7.style.display = "block"
+// 	let item8 = document.querySelector("#item8")
+// 	item8.style.display = "none"
+// 	let item9 = document.querySelector("#item9")
+// 	item9.style.display = "none"
+// 	let item10 = document.querySelector("#item10")
+// 	item10.style.display = "none"
+// 	let item11 = document.querySelector("#item11")
+// 	item11.style.display = "none"
+// 	let item12 = document.querySelector("#item12")
+// 	item12.style.display = "none"
+// 	let item13 = document.querySelector("#item13")
+// 	item13.style.display = "block"
+// 	let item14 = document.querySelector("#item14")
+// 	item14.style.display = "block"
+// 	let item15 = document.querySelector("#item15")
+// 	item15.style.display = "none"
+// 	let item16 = document.querySelector("#item16")
+// 	item16.style.display = "none"
+// 	let item17 = document.querySelector("#item17")
+// 	item17.style.display = "none"
+// 	let item18 = document.querySelector("#item18")
+// 	item18.style.display = "none"
+// 	let item19 = document.querySelector("#item19")
+// 	item19.style.display = "none"
+// 	let item20 = document.querySelector("#item20")
+// 	item20.style.display = "none"
+
+
+	discountbtn.style.background = "white"
+	womanbtn.style.background = "none"
+	popularbtn.style.background = "none"
+	allbtn.style.background = "none"
+	latestbtn.style.background = "none"
+	manbtn.style.background = "none"
+}
+
+let manbtn = document.querySelector("#btn5")
+manbtn.onclick = function () {
+	// let item1 = document.querySelector("#item1")
+	// item1.style.display = "block"
+	// let item2 = document.querySelector("#item2")
+	// item2.style.display = "block"
+	// let item3 = document.querySelector("#item3")
+	// item3.style.display = "block"
+	// let item4 = document.querySelector("#item4")
+	// item4.style.display = "block"
+	// let item5 = document.querySelector("#item5")
+	// item5.style.display = "block"
+	// let item6 = document.querySelector("#item6")
+	// item6.style.display = "block"
+	// let item7 = document.querySelector("#item7")
+	// item7.style.display = "block"
+	// let item8 = document.querySelector("#item8")
+	// item8.style.display = "block"
+	// let item9 = document.querySelector("#item9")
+	// item9.style.display = "block"
+	// let item10 = document.querySelector("#item10")
+	// item10.style.display = "block"
+	// let item11 = document.querySelector("#item11")
+	// item11.style.display = "none"
+	// let item12 = document.querySelector("#item12")
+	// item12.style.display = "none"
+	// let item13 = document.querySelector("#item13")
+	// item13.style.display = "none"
+	// let item14 = document.querySelector("#item14")
+	// item14.style.display = "none"
+	// let item15 = document.querySelector("#item15")
+	// item15.style.display = "none"
+	// let item16 = document.querySelector("#item16")
+	// item16.style.display = "none"
+	// let item17 = document.querySelector("#item17")
+	// item17.style.display = "none"
+	// let item18 = document.querySelector("#item18")
+	// item18.style.display = "none"
+	// let item19 = document.querySelector("#item19")
+	// item19.style.display = "none"
+	// let item20 = document.querySelector("#item20")
+	// item20.style.display = "none"
+
+
+	manbtn.style.background = "white"
+	womanbtn.style.background = "none"
+	popularbtn.style.background = "none"
+	allbtn.style.background = "none"
+	latestbtn.style.background = "none"
+	discountbtn.style.background = "none"
+}
+
+let womanbtn = document.querySelector("#btn6")
+womanbtn.onclick = function () {
+	// let item1 = document.querySelector("#item1")
+	// item1.style.display = "none"
+	// let item2 = document.querySelector("#item2")
+	// item2.style.display = "none"
+	// let item3 = document.querySelector("#item3")
+	// item3.style.display = "none"
+	// let item4 = document.querySelector("#item4")
+	// item4.style.display = "none"
+	// let item5 = document.querySelector("#item5")
+	// item5.style.display = "none"
+	// let item6 = document.querySelector("#item6")
+	// item6.style.display = "none"
+	// let item7 = document.querySelector("#item7")
+	// item7.style.display = "none"
+	// let item8 = document.querySelector("#item8")
+	// item8.style.display = "none"
+	// let item9 = document.querySelector("#item9")
+	// item9.style.display = "none"
+	// let item10 = document.querySelector("#item10")
+	// item10.style.display = "none"
+	// let item11 = document.querySelector("#item11")
+	// item11.style.display = "block"
+	// let item12 = document.querySelector("#item12")
+	// item12.style.display = "block"
+	// let item13 = document.querySelector("#item13")
+	// item13.style.display = "block"
+	// let item14 = document.querySelector("#item14")
+	// item14.style.display = "block"
+	// let item15 = document.querySelector("#item15")
+	// item15.style.display = "block"
+	// let item16 = document.querySelector("#item16")
+	// item16.style.display = "block"
+	// let item17 = document.querySelector("#item17")
+	// item17.style.display = "block"
+	// let item18 = document.querySelector("#item18")
+	// item18.style.display = "block"
+	// let item19 = document.querySelector("#item19")
+	// item19.style.display = "block"
+	// let item20 = document.querySelector("#item20")
+	// item20.style.display = "block"
+
+	womanbtn.style.background = "white"
+	manbtn.style.background = "none"
+	popularbtn.style.background = "none"
+	allbtn.style.background = "none"
+	latestbtn.style.background = "none"
+	discountbtn.style.background = "none"
+}
